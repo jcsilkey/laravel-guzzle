ac037691cd193a065121d5470a5468a6c1bb498d
Jason Silkey <jason@jasonsilkey.com>
2018-06-01 11:25:51 -0700

    add documentation

--- src/GuzzleClientRegistry.php
+++ src/GuzzleClientRegistry.php
@@ -9,27 +9,52 @@ use GuzzleHttp\HandlerStack;
 
 class GuzzleClientRegistry
 {
+    /**
+     * Collection of clients/client configurations
+     *
+     * @var (GuzzleHttp\ClientInterface|array)[]
+     */
     protected $clients;
 
+    /**
+     * Name of the default client
+     *
+     * @var string
+     */
     protected $default;
 
-    public function __construct(array $clientsConfiguration, string $default = 'default')
+    /**
+     * Constructor
+     *
+     * @param array $clientsConfiguration Configuration options for clients
+     * @param string $default The name of the default client
+     */
+    public function __construct(array $clientsConfiguration, string $default)
     {
         $this->clients = $clientsConfiguration;
 
         $this->default = $default;
     }
 
+    /**
+     * Get a client from the registry
+     *
+     * If no client name is passed, then the default client will be returned.
+     * The same client will be returned on subsequent requests.
+     *
+     * @param string|null $clientName The name of the client to get
+     *
+     * @return GuzzleHttp\ClientInterface The client requested
+     * @throws ClientNotRegisteredException If the requested client is not registered
+     */
     public function getClient(?string $clientName = null) : ClientInterface
     {
         $clientName = $clientName ?: $this->default;
 
-        if (isset($this->clients[$clientName]) && $this->clients[$clientName] instanceof Client) {
+        if (!isset($this->clients[$clientName])) {
+            throw new ClientNotRegisteredException($clientName);
+        } elseif ($this->clients[$clientName] instanceof ClientInterface) {
             return $this->clients[$clientName];
-        } elseif (!isset($this->clients[$clientName])) {
-            $client = new Client([]);
-
-            $this->clients[$clientName] = $client;
         } else {
             $config = $this->createHandlerStack($this->clients[$clientName]);
 
